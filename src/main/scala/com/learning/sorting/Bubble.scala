a3c2d2646ce2cd6674fdb721fce34cf4fb6d50c5
Thiago Nunes <thiagotnunes@gmail.com>
2016-12-26 22:18:45 +0100

    Adds new implementation of bubble sort

--- src/main/scala/com/learning/sorting/Bubble.scala
+++ src/main/scala/com/learning/sorting/Bubble.scala
@@ -9,6 +9,23 @@ package com.learning.sorting
   */
 object Bubble {
   def sort[T](xs: Array[T])(implicit o: Ordering[T]): Unit = {
+    var hasSwapped = false
+    var n = xs.length
+    do {
+      hasSwapped = false
+      for {
+        i <- Range(0, n - 1)
+      } yield {
+        if (o.gt(xs(i), xs(i + 1))) {
+          swap(xs, i, i + 1)
+          hasSwapped = true
+        }
+      }
+      n = n - 1
+    } while (hasSwapped)
+  }
+
+  def sort2[T](xs: Array[T])(implicit o: Ordering[T]): Unit = {
     for {
       i <- Range(xs.length - 1, 0, -1)
       j <- Range(0, i)
