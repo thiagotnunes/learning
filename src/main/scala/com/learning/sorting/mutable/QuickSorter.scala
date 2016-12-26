e52e8c19b60061dda6e8538faf59b85640e6ad0a
Thiago Nunes <thiagotnunes@gmail.com>
2016-12-27 00:17:46 +0100

    Adds quicksort

---
+++ src/main/scala/com/learning/sorting/mutable/QuickSorter.scala
@@ -0,0 +1,42 @@
+package com.learning.sorting.mutable
+
+import scala.util.Random
+
+/**
+  * - In place
+  * - NOT Stable
+  * - Time complexity:
+  *   - Best: O(nlogn)
+  *   - Average: O(nlogn)
+  *   - Worst: O(n^^2)
+  * - Space complexity: O(1)
+  */
+class QuickSorter extends Sorter {
+  override def sort[T](xs: Array[T])(implicit o: Ordering[T]): Unit = {
+    quickSort(xs, 0, xs.length - 1)
+  }
+
+  private def quickSort[T](xs: Array[T], lo: Int, hi: Int)(implicit o: Ordering[T]): Unit = {
+    if (lo < hi) {
+      val p = partition(xs, lo, hi)
+      quickSort(xs, lo, p - 1)
+      quickSort(xs, p + 1, hi)
+    }
+  }
+
+  private def partition[T](xs: Array[T], lo: Int, hi: Int)(implicit o: Ordering[T]): Int = {
+    val pivot = Random.nextInt(hi - lo) + lo
+    swap(xs, hi, pivot)
+
+    var i = lo
+    for {k <- Range(lo, hi)} yield {
+      if (o.lt(xs(k), xs(hi))) {
+        swap(xs, k, i)
+        i = i + 1
+      }
+    }
+
+    swap(xs, i, hi)
+    i
+  }
+}
---
+++ src/test/scala/com/learning/sorting/mutable/QuickSorterSpec.scala
@@ -0,0 +1,22 @@
+package com.learning.sorting.mutable
+
+import com.learning.PropertySpecification
+import org.scalacheck.Prop.forAll
+
+class QuickSorterSpec extends PropertySpecification {
+  val sorter: Sorter = new QuickSorter
+
+  "sorts the array in ascending order" in {
+    forAll { (xs: Array[Int]) =>
+      sorter.sort(xs)
+      xs ==== xs.sorted
+    }
+  }
+
+  "sorts the inverted order array" in {
+    val input = Array(10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
+    sorter.sort(input)
+
+    input ==== Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+  }
+}
