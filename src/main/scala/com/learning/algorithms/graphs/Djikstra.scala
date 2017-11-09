076c6aba9c4ee2967751143048fb7ff0e56eeec8
Thiago Nunes <thiagotnunes@gmail.com>
2017-11-09 21:56:10 +0100

    Adds djikstra algorithm

---
+++ src/main/scala/com/learning/algorithms/graphs/Djikstra.scala
@@ -0,0 +1,114 @@
+package com.learning.algorithms.graphs
+
+import com.learning.datastructures.mutable.graph.Graph
+import com.learning.datastructures.mutable.queue.PriorityQueue
+
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+
+class Djikstra {
+
+  /**
+    * - V is the number of vertices in the graph
+    * - E is the number of edges in the graph
+    *
+    * - Time Complexity - O(VlogV + ElogV)
+    * - Space Complexity - O(V)
+    */
+  def shortestPathWithPriorityQueue(graph: Graph)(source: Int, dest: Int): (Int, Seq[Int]) = {
+    val distances = mutable.Map[Int, Int]()
+    val previousNode = mutable.Map[Int, Int]()
+    val queue = new PriorityQueue[Int]()
+
+    val vertices = graph.allVertices // O(V)
+    vertices.foreach(v => queue.enqueue(v, Integer.MAX_VALUE)) // O(VlogV)
+    queue.decreasePriority(source, 0) // O(logV)
+
+    vertices.foreach(v => distances.put(v, Integer.MAX_VALUE)) // O(V)
+    distances.put(source, 0) // O(1)
+    previousNode.remove(source)
+
+    // O(VlogV + ElogV)
+    while (!queue.isEmpty) {
+      val vertex = queue.dequeue() // O(logV)
+
+      for (edge <- graph.edgesFrom(vertex)) { // In total E times
+
+        val vertexDistance = distances(vertex)
+        val currentDistance = distances(edge.to)
+
+        val relaxedDistance = vertexDistance + edge.weight
+        if (currentDistance > relaxedDistance) {
+          distances(edge.to) = relaxedDistance
+          previousNode.put(edge.to, vertex)
+          queue.decreasePriority(edge.to, relaxedDistance) // O(logV)
+        }
+      }
+    }
+
+    (
+      distances(dest),
+      buildPathFrom(previousNode, dest) // O(V)
+    )
+  }
+
+  /**
+    * - V is the number of vertices in the graph
+    * - E is the number of edges in the graph
+    *
+    * - Time Complexity - O(V^^2 + E)
+    * - Space Complexity - O(V)
+    */
+  def shortestPathWithoutPriorityQueue(graph: Graph)(source: Int, dest: Int): (Int, Seq[Int]) = {
+    val vertices = graph.allVertices
+    val distances = mutable.Map[Int, Int]()
+    val previousNode = mutable.Map[Int, Int]()
+    val visited = mutable.Set[Int]()
+
+    vertices.foreach(v => distances.put(v, Integer.MAX_VALUE)) // O(V)
+    distances.put(source, 0)
+
+    // O(V)
+    def nextMin(distances: mutable.Map[Int, Int], visited: mutable.Set[Int]): (Int, Int) = {
+      distances.foldLeft((Integer.MAX_VALUE, Integer.MAX_VALUE)) { case (acc, (v, distance)) =>
+          if (!visited.contains(v) && distance < acc._2) {
+            (v, distance)
+          } else {
+            acc
+          }
+      }
+    }
+
+    // O(V^2 + E)
+    for (_ <- vertices.indices) {
+      val (vertex, distance) = nextMin(distances, visited) // O(V)
+      visited.add(vertex) // O(1)
+
+      // In total O(E)
+      for (edge <- graph.edgesFrom(vertex)) {
+        val currentDistance = distances(edge.to)
+        val relaxedDistance = distance + edge.weight
+        if (relaxedDistance < currentDistance) {
+          previousNode(edge.to) = vertex
+          distances(edge.to) = relaxedDistance
+        }
+      }
+    }
+
+    (
+      distances(dest),
+      buildPathFrom(previousNode, dest)
+    )
+  }
+
+  // O(n)
+  private def buildPathFrom(previousNode: mutable.Map[Int, Int], dest: Int): Seq[Int] = {
+    val path = ArrayBuffer[Int]()
+    var node: Option[Int] = Some(dest)
+    while (node.isDefined) {
+      path.append(node.get)
+      node = previousNode.get(node.get)
+    }
+    path.reverse
+  }
+}
---
+++ src/test/scala/com/learning/algorithms/graphs/DjikstraSpec.scala
@@ -0,0 +1,62 @@
+package com.learning.algorithms.graphs
+
+import com.learning.datastructures.mutable.graph.{AdjacencyListGraph, Graph}
+import org.specs2.mutable.Specification
+import org.specs2.specification.Scope
+
+class DjikstraSpec extends Specification {
+
+  trait Context extends Scope {
+    val graph: Graph = new AdjacencyListGraph(9)
+    val djikstra = new Djikstra
+
+    graph.addEdge(0, 1, 4)
+    graph.addEdge(0, 7, 8)
+
+    graph.addEdge(1, 0, 4)
+    graph.addEdge(1, 2, 8)
+    graph.addEdge(1, 7, 11)
+
+    graph.addEdge(2, 1, 8)
+    graph.addEdge(2, 3, 7)
+    graph.addEdge(2, 5, 4)
+    graph.addEdge(2, 8, 2)
+
+    graph.addEdge(3, 2, 7)
+    graph.addEdge(3, 4, 9)
+    graph.addEdge(3, 5, 14)
+
+    graph.addEdge(4, 3, 9)
+    graph.addEdge(4, 5, 10)
+
+    graph.addEdge(5, 2, 4)
+    graph.addEdge(5, 3, 14)
+    graph.addEdge(5, 4, 10)
+
+    graph.addEdge(6, 5, 2)
+    graph.addEdge(6, 7, 1)
+    graph.addEdge(6, 8, 6)
+
+    graph.addEdge(7, 0, 8)
+    graph.addEdge(7, 1, 11)
+    graph.addEdge(7, 6, 1)
+    graph.addEdge(7, 8, 7)
+
+    graph.addEdge(8, 2, 2)
+    graph.addEdge(8, 6, 6)
+    graph.addEdge(8, 7, 7)
+  }
+
+  "using priority queues" in {
+    "returns the shortest path from source to destination" in new Context {
+      djikstra.shortestPathWithPriorityQueue(graph)(0, 4) ==== (21, Seq(0, 7, 6, 5, 4))
+    }
+  }
+
+  "without priority queues" in {
+    "returns the shortest path from source to destination" in new Context {
+      djikstra.shortestPathWithoutPriorityQueue(graph)(0, 8) ==== (14, Seq(0, 1, 2, 8))
+    }
+  }
+
+}
