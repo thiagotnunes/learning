e5e9e8e97cba307608cebd694c1eab01eb80697a
Thiago Nunes <thiagotnunes@gmail.com>
2017-12-03 19:19:15 +0100

    Adds bellman ford implementation

---
+++ src/main/scala/com/learning/algorithms/graphs/BellmanFord.scala
@@ -0,0 +1,61 @@
+package com.learning.algorithms.graphs
+
+import com.learning.datastructures.mutable.graph.Graph
+
+import scala.collection.mutable.ArrayBuffer
+
+/**
+  * Bellman-Ford algorithm does NOT work with negative cycles,
+  * but it does with negative edges.
+  *
+  * - V is the number of vertices in the graph
+  * - E is the number of edges in the graph
+  *
+  * Time Complexity - O(VE)
+  * Space Complexity - O(V)
+  */
+class BellmanFord {
+  def shortestPath(graph: Graph)(source: Int, dest: Int): (Int, Seq[Int]) = {
+    val distances = collection.mutable.Map[Int, Int]()
+    val previousNode = collection.mutable.Map[Int, Int]()
+    val vertices = graph.allVertices
+    val edges = graph.allEdges
+
+    vertices.foreach(v => {
+      distances.put(v, Integer.MAX_VALUE)
+      previousNode(v) = Integer.MAX_VALUE
+    })
+    distances.put(source, 0)
+    previousNode.put(source, Integer.MAX_VALUE)
+
+    for (_ <- 0.until(vertices.size - 1)) {
+      for (j <- edges.indices) {
+        val from = edges(j).from
+        val to = edges(j).to
+        val weight = edges(j).weight
+
+        if (distances(from) < Integer.MAX_VALUE && distances(to) > distances(from) + weight) {
+          distances.put(to, distances(from) + weight)
+          previousNode.put(to, from)
+        }
+      }
+    }
+
+    (
+      distances(dest),
+      buildPath(dest, previousNode)
+    )
+  }
+
+  // O(V)
+  private def buildPath(dest: Int, previousNode: collection.mutable.Map[Int, Int]): Seq[Int] = {
+    var current = dest
+    val path = ArrayBuffer[Int]()
+    do {
+      path.append(current)
+      current = previousNode(current)
+    } while (current != Integer.MAX_VALUE)
+
+    path.reverse
+  }
+}
---
+++ src/test/scala/com/learning/algorithms/graphs/BellmanFordSpec.scala
@@ -0,0 +1,64 @@
+package com.learning.algorithms.graphs
+
+import com.learning.datastructures.mutable.graph.AdjacencyListGraph
+import org.specs2.mutable.Specification
+
+class BellmanFordSpec extends Specification {
+
+  private val bellmanFord = new BellmanFord
+
+  "returns the shortest path" in {
+    val graph = new AdjacencyListGraph(9)
+    graph.addEdge(0, 1, 4)
+    graph.addEdge(0, 7, 8)
+
+    graph.addEdge(1, 0, 4)
+    graph.addEdge(1, 2, 8)
+    graph.addEdge(1, 7, 11)
+
+    graph.addEdge(2, 1, 8)
+    graph.addEdge(2, 3, 7)
+    graph.addEdge(2, 5, 4)
+    graph.addEdge(2, 8, 2)
+
+    graph.addEdge(3, 2, 7)
+    graph.addEdge(3, 4, 9)
+    graph.addEdge(3, 5, 14)
+
+    graph.addEdge(4, 3, 9)
+    graph.addEdge(4, 5, 10)
+
+    graph.addEdge(5, 2, 4)
+    graph.addEdge(5, 3, 14)
+    graph.addEdge(5, 4, 10)
+
+    graph.addEdge(6, 5, 2)
+    graph.addEdge(6, 7, 1)
+    graph.addEdge(6, 8, 6)
+
+    graph.addEdge(7, 0, 8)
+    graph.addEdge(7, 1, 11)
+    graph.addEdge(7, 6, 1)
+    graph.addEdge(7, 8, 7)
+
+    graph.addEdge(8, 2, 2)
+    graph.addEdge(8, 6, 6)
+    graph.addEdge(8, 7, 7)
+
+    bellmanFord.shortestPath(graph)(0, 4) ==== (21, Seq(0, 7, 6, 5, 4))
+  }
+
+  "returns the shortest path with negative edges" in {
+    val graph = new AdjacencyListGraph(4)
+
+    graph.addEdge(0, 1, 1)
+    graph.addEdge(0, 2, 0)
+    graph.addEdge(0, 3, 99)
+
+    graph.addEdge(1, 2, 1)
+
+    graph.addEdge(3, 1, -300)
+
+    bellmanFord.shortestPath(graph)(0, 2) ==== (-200, Seq(0, 3, 1, 2))
+  }
+}
